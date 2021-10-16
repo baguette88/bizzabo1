15b2ff55b5d62a9c64b2f19910fb07b0b25a6c42
jbrown7407 <54042125+jbrown7407@users.noreply.github.com>
2021-10-16 13:18:33 -0400

    Calendar control Function without API data

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-console.log('script start...')
+// console.log('script start...')
 let today = new Date();
 let hourNow = today.getHours();
 let greeting;
 
-if (hourNow > 18) {
+if (hourNow > 5 > 24) {
     greeting = "<b>Good Evening, User<b>";
     // add name as template literal
 }else if (hourNow >= 12) {
     greeting = "<b>Good Afternoon</b>";
-} else if (hourNow > 0 > 12) {
+}else if (hourNow > 0 > 12) {
     greeting = '<b>Good Morning</b>';
 } else { greeting = 'Welcome (calculator error occured)';
 }
@@ -18,9 +18,10 @@ document.getElementById('lblGreetings').innerHTML =
 
 console.log('pulled date successfully')
 
-$(document).ready(function () {
-    getEvents()
-})
+// $(document).ready(function () {
+//     getEvents()
+// })
+///REACTIVATE TO PERFORM API CALL
 
 
 $(document).ready(function(){
@@ -127,4 +128,86 @@ function getEvents() {
     }).done(function (data) {
         console.log(data);
     });
+}
+
+// let today = new Date();
+let currentMonth = today.getMonth();
+let currentYear = today.getFullYear();
+let selectYear = document.getElementById("year");
+let selectMonth = document.getElementById("month");
+
+let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+let monthAndYear = document.getElementById("monthAndYear");
+showCalendar(currentMonth, currentYear);
+
+
+function next() {
+    currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
+    currentMonth = (currentMonth + 1) % 12;
+    showCalendar(currentMonth, currentYear);
+}
+
+function previous() {
+    currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
+    currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
+    showCalendar(currentMonth, currentYear);
+}
+
+function jump() {
+    currentYear = parseInt(selectYear.value);
+    currentMonth = parseInt(selectMonth.value);
+    showCalendar(currentMonth, currentYear);
+}
+
+function showCalendar(month, year) {
+
+    let firstDay = (new Date(year, month)).getDay();
+    let daysInMonth = 32 - new Date(year, month, 32).getDate();
+
+    let tbl = document.getElementById("calendar-body"); // body of the calendar
+
+    // clearing all previous cells
+    tbl.innerHTML = "";
+
+    // filing data about month and in the page via DOM.
+    monthAndYear.innerHTML = months[month] + " " + year;
+    selectYear.value = year;
+    selectMonth.value = month;
+
+    // creating all cells
+    let date = 1;
+    for (let i = 0; i < 6; i++) {
+        // creates a table row
+        let row = document.createElement("tr");
+
+        //creating individual cells, filing them up with data.
+        for (let j = 0; j < 7; j++) {
+            if (i === 0 && j < firstDay) {
+                let cell = document.createElement("td");
+                let cellText = document.createTextNode("");
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            }
+            else if (date > daysInMonth) {
+                break;
+            }
+
+            else {
+                let cell = document.createElement("td");
+                let cellText = document.createTextNode(date);
+                if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
+                    cell.classList.add("bg-info");
+                } // color today's date
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+                date++;
+            }
+
+
+        }
+
+        tbl.appendChild(row); // appending each row into calendar body.
+    }
+
 }
