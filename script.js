b1e190d74d2fb0e6dfba65b665f19892d38646b9
jbrown7407 <54042125+jbrown7407@users.noreply.github.com>
2021-10-16 09:37:03 -0400

    troubleshooting ajax call, dynamic greeting complete

--- script.js
+++ script.js
@@ -3,13 +3,53 @@ let hourNow = today.getHours();
 let greeting;
 
 if (hourNow > 18) {
-    greeting = "Good Evening, User";
+    greeting = "<b>Good Evening, User<b>";
     // add name as template literal
 }else if (hourNow > 12) {
-    greeting = "Good Afternoon";
+    greeting = "<b>Good Afternoon</b>";
 } else if (hourNow > 0) {
-    greeting = 'Good Morning';
+    greeting = '<b>Good Morning</b>';
 } else { greeting = 'Welcome (calculator error occured)';
 }
 
-document.write(greeting);
+document.getElementById('lblGreetings').innerHTML =
+'<b>' + greeting + '</b> and Welcome to Bizzabo';
+
+
+//AJAX call
+function getEvents() {
+
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.bizzabo.com/api/events",
+        "method": "GET",
+        "Content-type": "application/x-www-form-urlencoded",
+        "headers": {
+            "accept": "application/vnd.bizzabo.v2.0+json",
+            "authorization": "Bearer b2f9b657-d8fd-4c34-a28b-eba13cab25c2",
+            "Access-Control-Allow-Origin": "*",
+        },
+        "data": "{}"
+    }
+
+//FIX AJAX CALL
+    ajax({
+        "url": "https://api.bizzabo.com/api/events",
+        "async": true,
+        "method": "GET",
+        "crossDomain": true,
+        "Referrer-Policy": "strict-origin-when-cross-origin",
+        "Content-type": "application/x-www-form-urlencoded",
+        "headers": {
+            "accept": "application/vnd.bizzabo.v2.0+json",
+            "authorization": "Bearer b2f9b657-d8fd-4c34-a28b-eba13cab25c2",
+            "Access-Control-Allow-Origin": "*",
+        }
+    }).done(function (data) {
+        console.log(data);
+    });
+}
+
+getEvents()
+//console.log(data)
